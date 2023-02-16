5a94a9cb66226d2533d778d20ae7205264674cd7
gaahvieira <gabrielapfvieira@gmail.com>
2023-02-16 20:58:08 -0300

    feature/creating the CPT class

--- wp-content/plugins/mv-slider/mv-slider.php
+++ wp-content/plugins/mv-slider/mv-slider.php
@@ -36,11 +36,35 @@ if( ! defined( 'ABSPATH' ) ){
 if( ! class_exists( 'MV_Slider' ) ){
     class MV_Slider{
         function __construct(){
+            this->define_constants();
+            required_once( MV_SLIDER_PATH . 'post-types/class.mv-slider-cpt.php' );
+            $MV_Slider_Post_Type = new MV_Slider_Post_Type;
+        }
 
+        public function define_constants(){
+            define( 'MV_SLIDER_PATH ', plugin_dir_path( __FILE__ ) );
+            define( 'MV_SLIDER_URL ', plugin_dir_url( __FILE__ ) );
+            define( 'MV_SLIDER_VERSION ', '1.0.0' );
+        }
+
+        public static function activate(){
+            update_option( 'rewrite_rules', '' );
+
+        }
+
+        public static function deactivate(){
+            flush_rewrite_rules();
+        }
+
+        public static function uninstall(){
         }
     }
 }
 
 if( class_exists( 'MV_Slider' ) ){
-    $vm_slider = new MV_Slider();
+    register_activation_hook( __FILE__, array( 'MV_Slider', 'activate' ) );
+    register_deactivation_hook( __FILE__, array( 'MV_Slider', 'deactivate' ) );
+    register_uninstall_hook( __FILE__, array( 'MV_Slider', 'uninstall' ) );
+
+    $mv_slider = new MV_Slider();
 }
---
+++ wp-content/plugins/mv-slider/post-types/class.mv-slider-cpt.php
@@ -0,0 +1,25 @@
+<?php
+
+if( !class_exists( 'MV_Slider_Post_Type' ) {
+    class MV_Slider_Post_Type{
+        function __construct(){
+            add_action( 'init', array( $this, 'create_post_type' ) );
+        }
+
+        public function create_post_type(){
+            register_post_type(
+                'mv-slider',
+                array(
+                    'label' => 'Slider',
+                    'description' => 'Sliders',
+                    'labels' => array(
+                        'name' => 'Sliders',
+                        'singular_name' -> 'Slider'
+                    ),
+                    'public' => true,
+
+                )
+            );
+        }
+    }
+}
