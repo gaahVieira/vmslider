24cadd1bd757b1464165900add32ad981bb784b9
gaahvieira <gabrielapfvieira@gmail.com>
2023-02-20 20:22:17 -0300

    feature/creating and register a new mvslider metabox

--- wp-content/plugins/mv-slider/mv-slider.php
+++ wp-content/plugins/mv-slider/mv-slider.php
@@ -19,45 +19,46 @@ MV Slider is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 any later version.
-
 MV Slider is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with MV Slider. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
 
-if( ! defined( 'ABSPATH' ) ){
+if( ! defined( 'ABSPATH') ){
     exit;
 }
 
 if( ! class_exists( 'MV_Slider' ) ){
     class MV_Slider{
         function __construct(){
-            this->define_constants();
-            required_once( MV_SLIDER_PATH . 'post-types/class.mv-slider-cpt.php' );
-            $MV_Slider_Post_Type = new MV_Slider_Post_Type;
+            $this->define_constants();
+
+            require_once( MV_SLIDER_PATH . 'post-types/class.mv-slider-cpt.php' );
+            $MV_Slider_Post_Type = new MV_Slider_Post_Type();
         }
 
         public function define_constants(){
-            define( 'MV_SLIDER_PATH ', plugin_dir_path( __FILE__ ) );
-            define( 'MV_SLIDER_URL ', plugin_dir_url( __FILE__ ) );
-            define( 'MV_SLIDER_VERSION ', '1.0.0' );
+            define( 'MV_SLIDER_PATH', plugin_dir_path( __FILE__ ) );
+            define( 'MV_SLIDER_URL', plugin_dir_url( __FILE__ ) );
+            define( 'MV_SLIDER_VERSION', '1.0.0' );
         }
 
         public static function activate(){
             update_option( 'rewrite_rules', '' );
-
         }
 
         public static function deactivate(){
             flush_rewrite_rules();
+            unregister_post_type( 'mv-slider' );
         }
 
         public static function uninstall(){
+
         }
+
     }
 }
 
@@ -67,4 +68,4 @@ if( class_exists( 'MV_Slider' ) ){
     register_uninstall_hook( __FILE__, array( 'MV_Slider', 'uninstall' ) );
 
     $mv_slider = new MV_Slider();
-}
+} 
--- wp-content/plugins/mv-slider/post-types/class.mv-slider-cpt.php
+++ wp-content/plugins/mv-slider/post-types/class.mv-slider-cpt.php
@@ -1,9 +1,10 @@
-<?php
+<?php 
 
-if( !class_exists( 'MV_Slider_Post_Type' ) {
+if( !class_exists( 'MV_Slider_Post_Type') ){
     class MV_Slider_Post_Type{
         function __construct(){
             add_action( 'init', array( $this, 'create_post_type' ) );
+            add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
         }
 
         public function create_post_type(){
@@ -11,15 +12,44 @@ if( !class_exists( 'MV_Slider_Post_Type' ) {
                 'mv-slider',
                 array(
                     'label' => 'Slider',
-                    'description' => 'Sliders',
+                    'description'   => 'Sliders',
                     'labels' => array(
-                        'name' => 'Sliders',
-                        'singular_name' -> 'Slider'
+                        'name'  => 'Sliders',
+                        'singular_name' => 'Slider'
                     ),
-                    'public' => true,
-
+                    'public'    => true,
+                    'supports'  => array( 'title', 'editor', 'thumbnail' ),
+                    'hierarchical'  => false,
+                    'show_ui'   => true,
+                    'show_in_menu'  => true,
+                    'menu_position' => 5,
+                    'show_in_admin_bar' => true,
+                    'show_in_nav_menus' => true,
+                    'can_export'    => true,
+                    'has_archive'   => false,
+                    'exclude_from_search'   => false,
+                    'publicly_queryable'    => true,
+                    'show_in_rest'  => true,
+                    'menu_icon' => 'dashicons-images-alt2',
+                    //'register_meta_box_cb'  =>  array( $this, 'add_meta_boxes' )
                 )
             );
         }
+
+        public function add_meta_boxes(){
+            add_meta_box(
+                'mv_slider_meta_box',
+                'Link Options',
+                array( $this, 'add_inner_meta_boxes' ),
+                'mv-slider',
+                'normal',
+                'high'
+            );
+        }
+
+        public function add_inner_meta_boxes( $post ){
+            require_once( MV_SLIDER_PATH . 'views/mv-slider_metabox.php' );
+        }
+
     }
 }
