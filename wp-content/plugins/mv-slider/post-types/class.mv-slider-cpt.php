e4a95c32464188108b716a51b177d06db51bd1ac
gaahvieira <gabrielapfvieira@gmail.com>
2023-02-21 21:09:46 -0300

    feature/validating user permissions and sanitazing fields

--- wp-content/plugins/mv-slider/post-types/class.mv-slider-cpt.php
+++ wp-content/plugins/mv-slider/post-types/class.mv-slider-cpt.php
@@ -5,6 +5,7 @@ if( !class_exists( 'MV_Slider_Post_Type') ){
         function __construct(){
             add_action( 'init', array( $this, 'create_post_type' ) );
             add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
+            add_action( 'save_post', array($this, 'save_post'), 10, 2);
         }
 
         public function create_post_type(){
@@ -51,5 +52,44 @@ if( !class_exists( 'MV_Slider_Post_Type') ){
             require_once( MV_SLIDER_PATH . 'views/mv-slider_metabox.php' );
         }
 
+        public function save_post( $post_id ){
+            if( isset( $_POST['mv_slider_nonce'] ) ){
+                if( ! wp_verify_nonce( $_POST['mv_slider_nonce'], 'mv_slider_nonce' ) ){
+                    return ;               
+                 }
+            }
+
+            if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ){
+                return ;
+            }
+
+            if( isset( $_POST['post_type'] ) && $_POST['post_type'] === 'mv-slider' ){
+                if( ! current_user_can( 'edit_page', $post_id ) ){
+                    return;
+                }elseif(! current_user_can( 'edit_post', $post_id ) ){
+                    return ;
+                }
+            }
+
+            if( isset( $_POST['action'] ) && $_POST['action'] == 'editpost' ){
+                $old_link_text = get_post_meta( $post_id, 'mv_slider_link_text', true );
+                $new_link_text = $_POST['mv_slider_link_text'];
+                $old_link_url = get_post_meta( $post_id, 'mv_slider_link_url', true );
+                $new_link_url =  $_POST['mv_slider_link_url'];
+
+                if( empty( $new_link_text )){
+                    updated_post_meta( $post_id, 'mv_slider_link_text', 'Add some text' );
+
+                }else{
+                    updated_post_meta( $post_id, 'mv_slider_link_text', sanitize_text_field( $new_link_text ),  $old_link_text );
+                }
+
+                if( empty( $new_link_url )){
+                    updated_post_meta( $post_id, 'mv_slider_link_url', '#');
+                }else{
+                    updated_post_meta( $post_id, 'mv_slider_link_url', esc_url_raw( $new_link_url ),  $old_link_url );
+                }
+            }
+        }
     }
 }
